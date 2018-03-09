4293e6ae06ddf6839fc0bd116af59257b1c6f852
Vo Ky Thoai <vokythoai@gmail.com>
2018-03-09 15:34:15 +0700

    Change price

--- app/service/parse_theme_service.rb
+++ app/service/parse_theme_service.rb
@@ -70,7 +70,7 @@ class ParseThemeService
                        {% assign total = 100 | minus: #{detail[1].to_i} | times: item.line_price | divided_by: 100 | plus: total  %}
                       </span>
                       <span class='after-discount-price'>
-                       {{ #{detail[1].to_i} | times: item.line_price | divided_by: 100 | money}}
+                       {{ 100 | minus: #{detail[1].to_i} | times: item.line_price | divided_by: 100 | money}}
                       </span>
                       </span>"
         end
